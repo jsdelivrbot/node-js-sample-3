9252beca969c29a296c7f300dedf7a271271f944
Sydney Garling <sydneygarling@Sydneys-MacBook-Pro.local>
2017-04-05 16:57:42 -0400

    lab9

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+function readFile (path) {
+  var fso = new ActiveXObject('Scripting.FileSystemObject'),
+      iStream=fso.OpenTextFile(path, 1, false);
+  while(!iStream.AtEndOfStream) {
+      document.body.innerHTML += iStream.ReadLine() + '<br/>';
+  }
+  iStream.Close();
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send(readFile("jfk.txt"));
 })
 
 app.listen(app.get('port'), function() {
