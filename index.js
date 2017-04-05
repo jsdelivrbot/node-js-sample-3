9073d9dd35293f48bfb0d9806207d5578035182e
Sydney Garling <sydneygarling@Sydneys-MacBook-Pro.local>
2017-04-05 19:38:46 -0400

    lab9

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var express = require('express')
 var app = express()
 
-var text = fs.readFileSync("jfk.txt", "utf-8");
+function readSingleFile(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var f = evt.target.files[0];
+  if(f){
+    var r = new FileReader();
+      r.onload = function(e) {
+	   var contents = e.target.result;
+       }
+    }
+}
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(text);
+  response.send(readSingleFile("jfk.txt"));
 })
 
 app.listen(app.get('port'), function() {
