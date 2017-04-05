a4ee9809a338652793871a5255d7b5db4031c3c4
Sydney Garling <sydneygarling@Sydneys-MacBook-Pro.local>
2017-04-05 19:21:51 -0400

    lab9

--- index.js
+++ index.js
@@ -5,33 +5,22 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 function readText(filePath) {
-        var output = ""; //placeholder for text output
-        if(filePath.files && filePath.files[0]) {
-            reader.onload = function (e) {
-                output = e.target.result;
-                displayContents(output);
-            };//end onload()
-            reader.readAsText(filePath.files[0]);
-        }//end if html5 filelist support
-        else if(ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
-            try {
-                reader = new ActiveXObject("Scripting.FileSystemObject");
-                var file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
-                output = file.ReadAll(); //text contents of file
-                file.Close(); //close file "input stream"
-                displayContents(output);
-            } catch (e) {
-                if (e.number == -2146827859) {
-                    alert('Unable to access local files due to browser security settings. ' +
-                     'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
-                     'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
-                }
-            }
-        }
-        else { //this is where you could fallback to Java Applet, Flash or similar
-            return false;
-        }
-        return true;
+  var files = evt.target.files;
+
+if (files) {
+  for (var i = 0, f; f = files[i]; i++) {
+      var r = new FileReader();
+      r.onload = (function (f) {
+          return function (e) {
+              var contents = e.target.result;
+              alert(contents);
+          };
+      })(f);
+      r.readAsText(f);
+  }
+} else {
+  alert("Failed to load files");
+}
 }
 
 app.get('/', function(request, response) {
