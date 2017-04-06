51deb8ee6338c0acea3aa37eb6d9f7c0b9474952
Sydney Garling <sydneygarling@Sydneys-MacBook-Pro.local>
2017-04-05 20:39:57 -0400

    lab9

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 var express = require('express');
 var app = express();
-
 var fs = require('fs');
 
-/*var fs = require('fs');
-var readline = require('readline');
-var stream = require('stream');
-
-var instream = fs.createReadStream('jfk.txt');
-var outstream = new stream;
-var rl = readline.createInterface(instream, outstream);
-
-rl.on('line', function(line) {
-});
-
-rl.on('close', function() {
-});*/
+try {
+    var data = fs.readFileSync('jkf.txt', 'utf8');
+    console.log(data);
+} catch(e) {
+    console.log('Error:', e.stack);
+}
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  fs.readFile('my-file.txt', 'utf8', function(err, data) {
-      if (err) throw err;
-      console.log(data);
-  });
   response.send(data);
 })
 
